cff5dce241c60495e164df0adf1f2798b569206b
Lukas Jungmann <lukas.jungmann@oracle.com>
2019-04-29 18:33:25 +0200

    jdk11 and OSGi related fixes
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -10,7 +10,7 @@
 
 module org.jvnet.staxex {
     requires java.logging;
-    requires java.xml.bind;
+    requires static java.xml.bind;
 
     requires transitive jakarta.activation;
     requires transitive java.xml;
