a2709a320324b04b43823dcea89791455743b1ac
Andy McCright <j.andrew.mccright@gmail.com>
2020-03-20 09:02:30 -0500

    [#29] Update to Jakarta EE 9 dependencies (#30)
    Signed-off-by: Andy McCright <j.andrew.mccright@gmail.com>
    
    Co-authored-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -10,7 +10,7 @@
 
 module org.jvnet.staxex {
     requires java.logging;
-    requires static java.xml.bind;
+    requires static jakarta.xml.bind;
 
     requires transitive jakarta.activation;
     requires transitive java.xml;
--- src/main/java/org/jvnet/staxex/StreamingDataHandler.java
+++ src/main/java/org/jvnet/staxex/StreamingDataHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -10,8 +10,8 @@
 
 package org.jvnet.staxex;
 
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
+import jakarta.activation.DataHandler;
+import jakarta.activation.DataSource;
 import java.io.BufferedInputStream;
 import java.io.Closeable;
 import java.io.File;
